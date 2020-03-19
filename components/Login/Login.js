80cd825a90f59571378f646c847582425e0b351c
Padunk <abraham.agel@gmail.com>
2020-03-19 10:09:15 +0700

    redesign login page

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,34 +1,91 @@
 import React from 'react';
-import {
-  Button,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 import Center from '../Center/Center';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const Login = ({navigation}) => {
+const Register = ({navigation}) => {
   return (
     <Center>
-      <View>
-        <Text>Email / UserName:</Text>
-        <TextInput placeholder="Email / Username" />
-        <Text>Password:</Text>
-        <TextInput secureTextEntry />
+      <View style={styles.form}>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.inputTitle}>Email:</Text>
+          <TextInput style={styles.input} placeholder="Email / Username" />
+        </View>
+        <View>
+          <Text style={styles.inputTitle}>Password:</Text>
+          <TextInput
+            style={styles.input}
+            secureTextEntry
+            placeholder="Password"
+          />
+        </View>
       </View>
-      <View>
-        <Text>New user?</Text>
-        <Button
-          title="Register"
-          onPress={() => navigation.navigate('Register')}
-        />
-      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Log in</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.inform}>
+          New user?{' '}
+          <Text
+            style={styles.link}
+            onPress={() => navigation.navigate('Register')}>
+            Register
+          </Text>
+        </Text>
+      </TouchableOpacity>
     </Center>
   );
 };
 
-export default Login;
+const styles = StyleSheet.create({
+  form: {
+    marginHorizontal: 40,
+    width: '100%',
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  inputWrapper: {
+    marginBottom: 20,
+  },
+  inputTitle: {
+    color: '#8560EB',
+    fontSize: 14,
+    textTransform: 'uppercase',
+    width: '100%',
+  },
+  input: {
+    borderBottomColor: '#F59FA2',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    height: 40,
+    fontSize: 18,
+    color: '#F59FA2',
+  },
+  button: {
+    height: 55,
+    marginHorizontal: 30,
+    marginBottom: 20,
+    backgroundColor: '#2865D6',
+    marginTop: 20,
+    padding: 10,
+    paddingLeft: 70,
+    paddingRight: 70,
+    fontSize: 24,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: '#efefef',
+    fontSize: 20,
+    fontWeight: 'bold',
+    letterSpacing: 1.34,
+  },
+  link: {
+    color: '#2865D6',
+  },
+  inform: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
+
+export default Register;
