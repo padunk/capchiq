64a08c6d4fdf9ef49ef25e04dfa9d61c316b1e69
Padunk <abraham.agel@gmail.com>
2020-03-26 17:10:12 +0700

    restyle home

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,18 +1,31 @@
 import React, {useContext} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+import Spacer from '../Spacer/Spacer'
 
 import {AuthContext} from '../AuthProvider/AuthProvider';
 
 const Home = () => {
-  const {logout} = useContext(AuthContext);
+  const {user, logout} = useContext(AuthContext);
   return (
     <>
       <View>
-        <Text>Home Page</Text>
+        <Spacer marginTop={50} />
+        <Text style={styles.title}>
+          Hi {user.displayName}!
+        </Text>
         <Button onPress={logout} title="Log out" />
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+
+  }
+})
 
 export default Home;
