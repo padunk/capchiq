c838081bba86432edfa63f1abc06ef6f76beb3fc
Padunk <abraham.agel@gmail.com>
2020-03-27 18:31:28 +0700

    feat(upload): upload user content

--- app/components/Upload/Upload.js
+++ app/components/Upload/Upload.js
@@ -1,8 +1,47 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useContext} from 'react';
+import 'react-native-get-random-values';
+import {v4 as uuidv4} from 'uuid';
+
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+
+import Center from '../Center/Center';
+import {AuthContext} from '../AuthProvider/AuthProvider';
 
 function Upload() {
-  return <Text>Upload</Text>;
+  const {user} = useContext(AuthContext);
+  const [description, setDescription] = useState('');
+
+  const postContent = () => {
+    try {
+      fetch(`http://192.168.168.17:6060/contents/${user.uid}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          content_id: uuidv4(),
+          content_description: description,
+          created_on: new Date(),
+          created_by: user.uid,
+        }),
+      });
+    } catch (error) {
+      // should return error to user
+    }
+  };
+
+  return (
+    <Center>
+      <TextInput
+        value={description}
+        onChangeText={text => setDescription(text)}
+      />
+      <TouchableOpacity onPress={postContent}>
+        <Text>Upload</Text>
+      </TouchableOpacity>
+    </Center>
+  );
 }
 
 export default Upload;
