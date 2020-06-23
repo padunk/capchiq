bd87c7cba32da1c82a88590d20710130a48e977b
Padunk <abraham.agel@gmail.com>
2020-06-23 16:25:51 +0700

    use env file

--- server/database/database.js
+++ server/database/database.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const userPool = new Pool({
-    user: "postgres",
-    password: "@Susilia121184",
-    host: "localhost",
-    port: 5432,
-    database: "capchiq",
+  user: process.env.POSTGRE_USER,
+  password: process.env.POSTGRE_PASSWORD,
+  host: "localhost",
+  port: 5432,
+  database: "capchiq",
 });
 
 module.exports = userPool;
