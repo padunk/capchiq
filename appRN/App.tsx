04f5e53e5dcf45f4bf92396aea30e14557e48507
Padunk <abraham.agel@gmail.com>
2020-09-21 11:49:27 +0700

    delete unnecassary import

--- appRN/App.tsx
+++ appRN/App.tsx
@@ -6,7 +6,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Layout from './components/Layout/Layout';
-import {Text} from 'react-native';
 import AuthProvider from './components/AuthProvider/AuthProvider';
 import Routes from './components/Routes/Routes';
 
