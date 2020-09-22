c158ad2ee9ddef888ecfc9e09a4cc5ddc654ba02
Padunk <abraham.agel@gmail.com>
2020-09-22 12:31:28 +0700

    re-compose

--- appRN/App.tsx
+++ appRN/App.tsx
@@ -1,22 +1,15 @@
 /**
- *
+ * @flow
  * @format
  */
 
-import 'react-native-gesture-handler';
-import React from 'react';
-import Layout from './components/Layout/Layout';
-import AuthProvider from './components/AuthProvider/AuthProvider';
-import Routes from './components/Routes/Routes';
+import * as React from 'react';
+import MainScreen from './components/Screens/MainScreen';
 
 const App = () => {
   return (
     <>
-      <AuthProvider>
-        <Layout>
-          <Routes />
-        </Layout>
-      </AuthProvider>
+      <MainScreen />
     </>
   );
 };
