d5899b541e8aaa6d644268278b98b9ce501d5a10
Padunk <abraham.agel@gmail.com>
2020-09-27 13:20:51 +0700

    get rid of set long time out warning

--- appRN/App.tsx
+++ appRN/App.tsx
@@ -5,8 +5,12 @@
 
 import * as React from 'react';
 import MainScreen from './components/Screens/MainScreen';
+import {LogBox} from 'react-native';
 
 const App = () => {
+  // ignoring warning about long set timeout due to using firebase I guess
+  // this is not the best work a round, but will do for now.
+  LogBox.ignoreLogs(['Setting a timer']);
   return (
     <>
       <MainScreen />
