d3d635d26df7361314796a4b537b476cee39c358
Padunk <abraham.agel@gmail.com>
2020-09-27 13:23:01 +0700

    adding storage and database

--- appRN/components/Firebase/Firebase.ts
+++ appRN/components/Firebase/Firebase.ts
@@ -1,13 +1,18 @@
 import * as firebase from 'firebase';
 import {firebaseConfig} from '../../secret';
 
+let firebaseStorage: firebase.storage.Storage;
+let firebaseDatabase: firebase.database.Database;
+
 try {
   if (firebase.apps.length === 0) {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+    firebaseStorage = firebase.storage();
+    firebaseDatabase = firebase.database();
   }
 } catch (error) {
   console.log('firebase error:', error);
 }
 
-export default firebase;
+export {firebase, firebaseDatabase, firebaseStorage};
