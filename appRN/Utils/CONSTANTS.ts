6f9a1f3f225487608bbd6afd726446b96173e7cb
Padunk <abraham.agel@gmail.com>
2020-10-02 17:34:11 +0700

    add dimensions

--- appRN/Utils/CONSTANTS.ts
+++ appRN/Utils/CONSTANTS.ts
@@ -1,4 +1,9 @@
+import {Dimensions} from 'react-native';
+
 // from SO https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 export const validEmailRegex = new RegExp(
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
 );
+
+export const {width: WIDTH} = Dimensions.get('window');
+export const {height: HEIGHT} = Dimensions.get('window');
