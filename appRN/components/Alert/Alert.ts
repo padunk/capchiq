35e9afd6588be13b0b4f0d5be5af1965a0f1dba7
Padunk <abraham.agel@gmail.com>
2020-10-02 17:32:38 +0700

    adding default state

--- appRN/components/Alert/Alert.ts
+++ appRN/components/Alert/Alert.ts
@@ -8,6 +8,19 @@ interface AlertProps {
 }
 
 const AlertComponent = ({title, msg, buttons, options}: AlertProps) => {
+  if (title === '' || title === undefined || title === null) {
+    return;
+  }
+  // default value if not provided
+  if (msg === '' || msg === undefined || msg === null) {
+    msg = '';
+  }
+  if (buttons === '' || buttons === undefined || buttons === null) {
+    buttons = [{text: 'OK', onPress: () => {}}];
+  }
+  if (options === '' || options === undefined || options === null) {
+    options = {};
+  }
   return Alert.alert(title, msg, buttons, options);
 };
 
