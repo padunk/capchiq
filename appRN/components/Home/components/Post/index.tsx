8c7764d25dc627b5d4da01e9ddfeee7fbbc5e479
Padunk <abraham.agel@gmail.com>
2020-10-06 16:37:22 +0700

    implement user detail navigation

--- appRN/components/Home/components/Post/index.tsx
+++ appRN/components/Home/components/Post/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
 import PostBody from './Body';
 import PostFooter from './Footer';
 import PostHeader from './Header';
+import {UserContext} from '../../../Provider/UserProvider';
 
-const Post = (props) => {
+const Post = (props: any) => {
+  const navigation = useNavigation();
+  const {setUserID} = useContext(UserContext);
+
   return (
     <View>
-      <PostHeader
-        photoURL={props.user.photoURL}
-        displayName={props.user.displayName}
-      />
-      <PostBody uri={props.video.uri} />
+      <TouchableOpacity
+        onPress={() => {
+          setUserID(props.user.id);
+          navigation.navigate('UserDetail');
+        }}>
+        <PostHeader
+          photoURL={props.user.photoURL}
+          displayName={props.user.displayName}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('PostDetail')}>
+        <PostBody uri={props.video.uri} />
+      </TouchableOpacity>
       <PostFooter likeCount={props.video.likeCount} title={props.video.title} />
     </View>
   );
