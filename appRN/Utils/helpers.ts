2f1f861d7a99d289baca467489eb64f54ad76ac5
Padunk <abraham.agel@gmail.com>
2020-10-06 16:38:38 +0700

    add monthyear function

--- appRN/Utils/helpers.ts
+++ appRN/Utils/helpers.ts
@@ -1,3 +1,18 @@
+const months = {
+  0: 'January',
+  1: 'February',
+  2: 'March',
+  3: 'April',
+  4: 'May',
+  5: 'June',
+  6: 'July',
+  7: 'August',
+  8: 'September',
+  9: 'October',
+  10: 'November',
+  11: 'December',
+};
+
 const toUpperCase = (str: string): string => str.toUpperCase();
 const toLowerCase = (str: string): string => str.toLowerCase();
 const concatString = (source: string, ...target: string[]): string =>
@@ -5,3 +20,9 @@ const concatString = (source: string, ...target: string[]): string =>
 
 export const capitalizeFirstLetter = (str: string): string =>
   concatString(toUpperCase(str.slice(0, 1)), toLowerCase(str.slice(1)));
+
+const getMonth = (str: string): number => new Date(str).getMonth();
+const getYear = (str: string): number => new Date(str).getFullYear();
+
+export const toMonthYear = (str: string): string =>
+  `${months[getMonth(str)]}  ${getYear(str)}`;
