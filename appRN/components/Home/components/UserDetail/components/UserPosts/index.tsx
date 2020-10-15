5c407c42ee298ad96a9325cc44bbcc56082e273b
Padunk <abraham.agel@gmail.com>
2020-10-15 10:20:04 +0700

    feat(): show poster of videos

--- appRN/components/Home/components/UserDetail/components/UserPosts/index.tsx
+++ appRN/components/Home/components/UserDetail/components/UserPosts/index.tsx
@@ -1,70 +1,90 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   ActivityIndicator,
   FlatList,
+  Pressable,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {createThumbnail} from 'react-native-create-thumbnail';
 import Video from 'react-native-video';
+
 import {WIDTH} from '../../../../../../Utils/CONSTANTS';
 import {firebaseDatabase} from '../../../../../Firebase/Firebase';
 import {COLOR} from '../../../../../Style/styles';
 
 const UserPosts = ({userData}) => {
   const [postContents, updatePostContents] = useState<any[] | null>(null);
+  // const [poster, setPoster] = useState<string | undefined>();
 
   useEffect(() => {
     async function getPosts(id: string) {
-      const videosSnapshot: firebase.database.DataSnapshot = await firebaseDatabase
-        .ref('/videos/' + id)
-        .limitToFirst(24)
-        .once('value');
+      try {
+        const videosSnapshot: firebase.database.DataSnapshot = await firebaseDatabase
+          .ref('/videos/' + id)
+          .limitToFirst(24)
+          .once('value');
 
-      const videosObject = videosSnapshot.val();
+        const videosObject = videosSnapshot.val();
 
-      let result = [];
-      for (const video in videosObject) {
-        if (Object.prototype.hasOwnProperty.call(videosObject, video)) {
-          const element = videosObject[video];
-          result.push(element);
+        let result = [];
+        for (const video in videosObject) {
+          if (Object.prototype.hasOwnProperty.call(videosObject, video)) {
+            const element = videosObject[video];
+
+            // add video poster
+            // element.poster = await createThumbnail({
+            //   // url: element.uri,
+            //   url: '../../../../../../assets/videos/50-beaches.mp4',
+            //   timeStamp: 1000,
+            // });
+            // console.log('element :>> ', element);
+            result.push(element);
+          }
         }
-      }
-      // testing
-      for (let i = 0; i < 23; i++) {
-        let clone = Object.assign({}, JSON.parse(JSON.stringify(result[0])), {
-          timestamp: i,
-        });
-        result.push(result[0]);
-      }
+        // testing
+        // for (let i = 0; i < 11; i++) {
+        //   let clone = Object.assign({}, JSON.parse(JSON.stringify(result[0])), {
+        //     timestamp: i,
+        //   });
+        //   result.push(clone);
+        // }
 
-      updatePostContents(result);
+        updatePostContents(result);
+      } catch (error) {
+        console.log('Error loading posts :>> ', error);
+      }
     }
 
     getPosts(userData.id);
   }, []);
 
-  const renderPosts = ({item}) => {
+  const videoPlayer = useRef<Video | null>(null);
+  const renderPosts = ({item}: any) => {
     return (
-      <TouchableOpacity onPress={() => console.log('post detail')}>
+      <Pressable onPress={() => console.log('post detail')}>
         <Video
+          // source={{uri: item.uri}}
           source={require('../../../../../../assets/videos/50-beaches.mp4')}
           style={{
             width: WIDTH / 3,
             height: WIDTH / 3,
             backgroundColor: COLOR.black,
           }}
-          paused
+          ref={(ref) => (videoPlayer.current = ref)}
           muted
+          paused
+          // poster={item.poster.path}
+          posterResizeMode="contain"
           resizeMode="contain"
         />
-      </TouchableOpacity>
+      </Pressable>
     );
   };
 
   return (
-    <>
+    <View>
       {postContents === null ? (
         <ActivityIndicator size="large" />
       ) : postContents.length === 0 ? (
@@ -80,7 +100,7 @@ const UserPosts = ({userData}) => {
           horizontal={false}
         />
       )}
-    </>
+    </View>
   );
 };
 
