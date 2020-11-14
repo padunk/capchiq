6e9f7a8565ef95db852644324189f391372acdd5
Padunk <abraham.agel@gmail.com>
2020-11-14 11:58:08 +0700

    test data

--- appRN/components/Settings/Settings.tsx
+++ appRN/components/Settings/Settings.tsx
@@ -1,11 +1,12 @@
 import React, {useContext} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, Image} from 'react-native';
 import {AuthContext} from '../AuthProvider/AuthProvider';
 
 function Settings() {
   const {user, logout} = useContext(AuthContext);
   return (
     <>
+      <Text>{user?.public.displayName}</Text>
       <Button onPress={logout} title="Log out" />
     </>
   );
