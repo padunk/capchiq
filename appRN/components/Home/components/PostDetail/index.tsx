e3b2e7af1dfd993db3259464ffd4c75d24c6c316
Padunk <abraham.agel@gmail.com>
2020-11-17 17:20:14 +0700

    video detail components

--- appRN/components/Home/components/PostDetail/index.tsx
+++ appRN/components/Home/components/PostDetail/index.tsx
@@ -1,14 +1,124 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Video from 'react-native-video';
+import {WIDTH} from '../../../../Utils/CONSTANTS';
+import {VideoContext} from '../../../Provider/VideoProvider';
+import {UserContext} from '../../../Provider/UserProvider';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import {firebaseDatabase} from '../../../Firebase/Firebase';
+import {UserPublicData} from '../../../../Types/types';
+import PostHeader from '../Post/Header';
+import {COLOR, globalStyles} from '../../../Style/styles';
 
-const PostDetail = (props) => {
+const PostDetail = () => {
+  const {userID} = useContext(UserContext);
+  const {videoDetail} = useContext(VideoContext);
+  const [userData, setUserData] = useState<UserPublicData | null>(null);
+  const [videoDimension, setVideoDimension] = React.useState({
+    width: 0,
+    height: 0,
+  });
+  // {uri: videoDetail.uri}
+
+  useEffect(() => {
+    async function getUserData(id: string) {
+      const idolSnapshot = await firebaseDatabase
+        .ref('/users/' + id)
+        .child('public')
+        .once('value');
+
+      setUserData(idolSnapshot.exportVal());
+    }
+
+    getUserData(userID);
+  }, [userID]);
+
   return (
     <View>
-      <Text>Post Detail</Text>
+      {userData !== null && <PostHeader user={userData} />}
+      <Video
+        style={{
+          width: videoDimension.width,
+          height: videoDimension.height,
+        }}
+        muted
+        source={require('../../../../assets/videos/easy.mp4')}
+        resizeMode="contain"
+        controls={true}
+        repeat={true}
+        onLoad={(data) => {
+          //   console.log('data', data);
+          const scale = WIDTH / data.naturalSize.width;
+          setVideoDimension((prevState) => {
+            return {
+              ...prevState,
+              width: data.naturalSize.width * scale,
+              height: data.naturalSize.height * scale,
+            };
+          });
+        }}
+      />
+
+      <View style={styles.info}>
+        <View style={styles.videoInfoBar}>
+          <SimpleLineIcons name="fire" size={24} color="red" />
+          <Text style={styles.likeCount}>{videoDetail?.likeCount}</Text>
+        </View>
+        <View>
+          <Text>{videoDetail?.timestamp}</Text>
+        </View>
+      </View>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>
+          <Text style={globalStyles.strong}>{userData?.displayName}: </Text>
+          {videoDetail?.title}
+        </Text>
+      </View>
+
+      <View style={styles.comments}>
+        <Text>Comments:</Text>
+      </View>
     </View>
   );
 };
 
 export default PostDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  titleContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    borderBottomColor: COLOR.grayColor,
+    borderBottomWidth: 1,
+    borderTopColor: COLOR.grayColor,
+    borderTopWidth: 1,
+  },
+  title: {
+    fontSize: 20,
+  },
+  info: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  videoInfoBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  likeCount: {
+    marginLeft: 10,
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  comments: {
+    marginTop: 16,
+    marginLeft: 4,
+    borderBottomColor: COLOR.grayColor,
+    borderBottomWidth: 1,
+  },
+});
